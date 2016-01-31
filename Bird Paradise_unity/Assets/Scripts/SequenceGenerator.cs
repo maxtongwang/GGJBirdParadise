fc2b2d129badae4c11a44ee0d898f9190ef51dc1
Jacob Baker <jsbaker73@gmail.com>
2016-01-30 16:40:43 -0800

    changed Sequence Generator

--- Bird Paradise_unity/Assets/Scripts/SequenceGenerator.cs
+++ Bird Paradise_unity/Assets/Scripts/SequenceGenerator.cs
@@ -12,6 +12,8 @@ public class SequenceGenerator : MonoBehaviour
 													new char[] {'I','K','M'},
 													new char[] {'U','J','N'},
 													new char[] {'Y','H','B'}};
+
+	private static int round = 0;
 	
 	public static void NewRound(ref char[] p1Letters, ref char[] p2Letters)
 	{
@@ -22,44 +24,65 @@ public class SequenceGenerator : MonoBehaviour
 		p1Letters = new char[length];
 		p2Letters = new char[length];
 
-		int[][] sequence = new int[][] {new int[] {1, 1, 1, 1, 1, 1},
-										new int[] {0, 1, 2, 0, 1, 2}};
+		int[][] sequence = new int[][] {new int[] {0, 1, 2, 0, 1, 2},
+										new int[] {1, 1, 1, 1, 1, 1}};
 		//sequence = GenerateTheSequence(length, difficulty);
 
 		p1Letters = InterpretSequence(sequence, 0, length, true);
 		p2Letters = InterpretSequence(sequence, length, length, false);
+
+		round += 1;
 	}
 
 	static int ChooseDificulty()
 	{
 		int difficulty = 0;
 
+
+
 		return difficulty;
 	}
 
 	static int[][] GenerateTheSequence(int length, int difficulty)
 	{
-		int[][] sequence = new int[2][];
+		int[][] sequence;
 
 		switch(difficulty % 3)
 		{
 		case 0:
+			sequence = TotalControl (length);
 			break;
 		case 1:
+			sequence = TotalControl (length);
 			break;
 		case 2:
+			sequence = TotalControl (length);
 			break;
 		}
 
 		return sequence;
 	}
 
-	static int[] WildAndRandom(int length, int size)
+	static int[][] TotalControl(int length)
 	{
-		int[] sequence = new int[length];
+		int[][] sequence = new int[2][length];
 
-		for(int i = 0; i < length; i += 1)
-		{	sequence[i] = Random.Range(0, size-1);	}
+
+	}
+
+	static int[][] StructuredRandom(int length)
+	{
+		int[][] sequence = new int[2][length];
+
+
+	}
+
+	static int[][] WildAndRandom(int length)
+	{
+		int[][] sequence = new int[2][length];
+
+//		for(int i = 0; i < length; i += 1)
+//		{	sequence[i] = Random.Range(0, size-1);	}
 
 		return sequence;
 	}
@@ -71,9 +94,9 @@ public class SequenceGenerator : MonoBehaviour
 		for(int i = start; i < start + length; i += 1)
 		{
 			if (p1)
-			{	letterSequence[i] = p1Keys[sequence[1][i]][sequence[0][i]];	}
+			{	letterSequence[i] = p1Keys[sequence[0][i]][sequence[1][i]];	}
 			else
-			{	letterSequence[i-start] = p2Keys[sequence[1][i]][sequence[0][i]];	}
+			{	letterSequence[i-start] = p2Keys[sequence[0][i]][sequence[1][i]];	}
 		}
 
 		return letterSequence;
